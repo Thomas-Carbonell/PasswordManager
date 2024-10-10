2a9272732bc2c3c2fe726adcb0ebab0674cdf6ab
Thomas Carbonell <t.carbonell@lancaster.ac.uk>
2024-10-10 23:18:28 +0200

    Slight adjustment

--- Main.java
+++ Main.java
@@ -1,19 +1,22 @@
 import java.util.List;
 /**
  * TODO//
+ * Improve UI
+ * Allow ability to select which kinds of characters are used in the password
+ * Allow ability to edit given passwords
+ * Allow ability to add values to the PC clipboard.
  * Comment all current code with Javadoc comments
  * Consider the following:
  * Password Storage
  * Accounts linked to Passwords
  * Encryption for stored passwords
- * Finally - GUI
  */
 public class Main {
     public static void main(String [] args) {
 
         List<Character> charArray = null;
         try {
-            readTxt file = new readTxt("AllowedCharacters.txt");
+            readTxt file = new readTxt("AllowedCharacters.txt"); 
             charArray = file.readFile();
             GeneratePassword pass = new GeneratePassword(10, charArray);
             userInterface ui = new userInterface(pass);
